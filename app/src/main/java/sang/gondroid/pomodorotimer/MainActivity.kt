11ddb9252e9aa2a31965ff097f930f4b44e1af05
GONI95 <sanggoni009@gmail.com>
2022-03-17 17:08:22 +0900

    Feat: SeekBar를 이용해 TextView 변경

--- app/src/main/java/sang/gondroid/pomodorotimer/MainActivity.kt
+++ app/src/main/java/sang/gondroid/pomodorotimer/MainActivity.kt
@@ -2,10 +2,30 @@ package sang.gondroid.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import sang.gondroid.pomodorotimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.controlMinutesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.remainMinutesTextView.text = resources.getString(R.string.remainMinutesFormat, progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) { }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) { }
+        })
     }
 }
