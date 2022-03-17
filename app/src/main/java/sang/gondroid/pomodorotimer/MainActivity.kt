259df257aedd458112aafac788afd52e3717c046
GONI95 <sanggoni009@gmail.com>
2022-03-17 20:05:32 +0900

    Feat: SoundPool을 이용한 ticking, bell Sound 재생

--- app/src/main/java/sang/gondroid/pomodorotimer/MainActivity.kt
+++ app/src/main/java/sang/gondroid/pomodorotimer/MainActivity.kt
@@ -1,9 +1,9 @@
 package sang.gondroid.pomodorotimer
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.widget.SeekBar
 import sang.gondroid.pomodorotimer.databinding.ActivityMainBinding
 
@@ -14,15 +14,39 @@ class MainActivity : AppCompatActivity() {
     // Gon : SeekBar를 조작할 때 마다 CountDownTimer 새로 생성하기 때문에 null인 경우에만 생성하기 위해서 선언
     private var currentCountDownTimer: CountDownTimer? = null
 
+    private val soundPoll = SoundPool.Builder().build()
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initViews()
+        init()
     }
 
-    private fun initViews() = with(binding) {
+    override fun onResume() {
+        super.onResume()
+
+        soundPoll.autoResume()
+    }
+
+    // Gon : 사운드는 특정 앱에서만 리소스를 가지는 것이 아님, 디바이스에서 가지는 사운드에 재생을 요청하기 때문에 앱에서 멈추도록 처리
+   override fun onPause() {
+        super.onPause()
+
+        soundPoll.autoPause()
+    }
+
+    // Gon : 앱을 사용하지 않는 경우 리소스를 해제(soundPoll에 로드외었던 파일들이 해제됨)
+    override fun onDestroy() {
+        super.onDestroy()
+
+        soundPoll.release()
+    }
+
+    private fun init() = with(binding) {
 
         /*
         Gon : onStopTrackingTouch()로 CountDownTimer 객체가 생성되고 updateSeekBar()를 통해
@@ -45,10 +69,12 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 if (seekBar == null) return
 
-                currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L).start()
-                currentCountDownTimer?.start()
+                startCountDown(seekBar)
             }
         })
+
+        tickingSoundId = soundPoll.load(this@MainActivity, R.raw.timer_tick, 1)
+        bellSoundId = soundPoll.load(this@MainActivity, R.raw.timer_bell, 1)
     }
 
     // Gon : 종료까지 남은 밀리초, onTick() 메서드 호출 간격
@@ -63,6 +89,11 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                 updateRemainTime(0)
                 updateSeekBar(0)
+
+                soundPoll.autoPause()
+                bellSoundId?.let {
+                    soundPoll.play(it, 1F, 1F, 0, 0, 1F)
+                }
             }
         }
 
@@ -77,5 +108,26 @@ class MainActivity : AppCompatActivity() {
     private fun updateSeekBar(millis: Long) = with(binding) {
         // Gon : 남은 전체 밀리초로 받기 때문에 분 단위의 SeekBar를 표현하기 위해서 60000(1분)으로 값 처리
         controlMinutesSeekBar.progress = (millis / 60000).toInt()
+    }
+
+    private fun startCountDown(seekBar: SeekBar) {
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L).start()
+        currentCountDownTimer?.start()
+
+        //Gon : loop forever -> -1
+        tickingSoundId?.let {
+            soundPoll.play(it, 1F, 1F, 0, -1, 1F)
+        }
+    }
+
+
+    private fun completeCountDown() {
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPoll.autoPause()
+        bellSoundId?.let {
+            soundPoll.play(it, 1F, 1F, 0, 0, 1F)
+        }
     }
 }
