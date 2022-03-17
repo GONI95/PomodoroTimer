17e8a7c1f427445fa43b0e8abddb579e0d40b831
GONI95 <sanggoni009@gmail.com>
2022-03-17 17:52:14 +0900

    Feat: CountDownTimer 사용
    CountDownTimer와 SeekBar를 연결하여 SeekBar 조작에 대해 CountDown이 되도록 구현
    currentCountDownTimer 프로퍼티를 이용해 onStartTrackingTouch(), onStopTrackingTouch()를 구현하여 조작 시 매번 CountDownTimer를 생성되는 부분을 해결

--- app/src/main/java/sang/gondroid/pomodorotimer/MainActivity.kt
+++ app/src/main/java/sang/gondroid/pomodorotimer/MainActivity.kt
@@ -2,12 +2,16 @@ package sang.gondroid.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import sang.gondroid.pomodorotimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    // Gon : SeekBar를 조작할 때 마다 CountDownTimer 새로 생성하기 때문에 null인 경우에만 생성하기 위해서 선언
+    private var currentCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,15 +21,51 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
-    private fun initViews() {
-        binding.controlMinutesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+    private fun initViews() = with(binding) {
+        controlMinutesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                binding.remainMinutesTextView.text = resources.getString(R.string.remainMinutesFormat, progress)
+                updateRemainTime(progress * 60 * 1000L)
             }
 
-            override fun onStartTrackingTouch(p0: SeekBar?) { }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                currentCountDownTimer?.cancel()
+                currentCountDownTimer = null
+            }
 
-            override fun onStopTrackingTouch(p0: SeekBar?) { }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar == null) return
+
+                currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L).start()
+                currentCountDownTimer?.start()
+            }
         })
+    }
+
+    // Gon : 종료까지 남은 밀리초, onTick() 메서드 호출 간격
+    private fun createCountDownTimer(millis: Long): CountDownTimer =
+        object: CountDownTimer(millis, 1000L) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                updateRemainTime(0)
+                updateSeekBar(0)
+            }
+        }
+
+    private fun updateRemainTime(millis: Long) = with(binding) {
+        val remainSeconds = millis / 1000
+
+        // Gon : 남은 전체 시간을 초로 받기 때문에 분,초를 표현하기 위해서 60(1분)으로 값 처리
+        remainMinutesTextView.text = resources.getString(R.string.minutesFormat, remainSeconds / 60)
+        remainSecondsTextView.text = resources.getString(R.string.secondsFormat, remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(millis: Long) = with(binding) {
+        // Gon : 남은 전체 밀리초로 받기 때문에 분 단위의 SeekBar를 표현하기 위해서 60000(1분)으로 값 처리
+        controlMinutesSeekBar.progress = (millis / 60000).toInt()
     }
 }
